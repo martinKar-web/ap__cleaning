c2ddba8677cfa3ad62cbf8ff46bea47318208e73
Martin Karugaba <martinkarugaba21@gmail.com>
2021-06-08 15:47:35 +0300

    completed why us section

--- js/swiper.js
+++ js/swiper.js
@@ -1,10 +1,10 @@
-//...
-const swiper = new Swiper('.swiper-container', {
-  speed: 2000,
+//***************** slide1
+const swiper_1 = new Swiper('.swiper1', {
+  speed: 500,
   spaceBetween: 0,
   centeredSlides: true,
   autoplay: {
-    delay: 5000,
+    delay: 3000,
     disableOnInteraction: true,
   },
   loop: true,
@@ -12,6 +12,17 @@ const swiper = new Swiper('.swiper-container', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  effect: 'fade',
+  effect: 'slide',
   initialSlide: 0,
+});
+
+//***************** slide 2
+const swiper_2 = new Swiper('.swiper2', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 });
