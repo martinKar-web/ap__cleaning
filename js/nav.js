6f1144d4a03d77dd40d27de87814aa6f3178a059
Martin Karugaba <martinkarugaba21@gmail.com>
2021-08-18 23:58:28 +0300

    made header, nav, hero, cards, whyUs tablet responsive

--- js/nav.js
+++ js/nav.js
@@ -2,10 +2,15 @@ const burger = document.querySelector('.burger');
 const nav_list = document.querySelector('.nav-list');
 const nav_links = document.querySelectorAll('.nav-link');
 
-console.log(nav_links);
+// burger lines
+const burgerTopLine = document.querySelector('.burger__line1')
+const burgerMiddleLine = document.querySelector('.burger__line2')
+const burgerBottomLine = document.querySelector('.burger__line3')
 
 burger.addEventListener('click', () => {
   nav_list.classList.toggle('active');
+  burgerTopLine.classList.toggle('topLine');
+  burgerBottomLine.classList.toggle('bottomLine');
 })
 
 nav_links.forEach(nav_link => {
